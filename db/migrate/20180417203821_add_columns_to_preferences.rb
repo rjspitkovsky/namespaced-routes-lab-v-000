1d2b56310162f224afc1e0b41f31169ba988827d
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-04-17 20:43:00 +0000

    automatically backed up by learn

--- db/migrate/20180417203821_add_columns_to_preferences.rb
+++ db/migrate/20180417203821_add_columns_to_preferences.rb
@@ -1,6 +1,6 @@
 class AddColumnsToPreferences < ActiveRecord::Migration
   def change
-    add_column :preferences, :allow_create_artists, :boolean
-    add_column :preferences, :allow_create_songs, :boolean 
+    add_column :preferences, :allow_create_artists, :boolean, :default => false
+    add_column :preferences, :allow_create_songs, :boolean, :default => true  
   end
 end
